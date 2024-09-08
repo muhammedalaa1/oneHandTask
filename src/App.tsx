ab808c23087fe1ac5b962d8103dccdd8a6ae191a
mohammed alaa ali mohammed <muhammedalaa2213@gmail.com>
2024-09-08 11:31:19 +0300

    feat : route protections

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useNavigate } from "react-router-dom";
 import Home from "./components/Home";
 import Login from "./components/Login";
 import Nav from "./components/Nav";
+import { useEffect } from "react";
 
 function App() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const token = document.cookie
+      .split("; ")
+      .find((row) => row.startsWith("token="))
+      ?.split("=")[1];
+    console.log(token);
+
+    if (token !== "one-hand1234") {
+      navigate("/login");
+    } else {
+      navigate("/home");
+    }
+  }, []);
   return (
     <>
       <Nav />
